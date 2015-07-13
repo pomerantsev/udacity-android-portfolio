7d3fe0f162f57b4b1e9e3e7da3b22c46ca972a09
Pavel Pomerantsev <pomerantsevp@gmail.com>
2015-07-13 19:54:21 +0300

    Remove unused menu

--- app/src/main/java/udacity/pomerantsevp/ru/portfolio/MyAppPortfolio.java
+++ app/src/main/java/udacity/pomerantsevp/ru/portfolio/MyAppPortfolio.java
@@ -2,8 +2,6 @@ package udacity.pomerantsevp.ru.portfolio;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
 public class MyAppPortfolio extends ActionBarActivity {
@@ -14,25 +12,4 @@ public class MyAppPortfolio extends ActionBarActivity {
         setContentView(R.layout.activity_my_app_portfolio);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_my_app_portfolio, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
